08960785d539c60e61e74191114a131a02d976e6
Tom Murray <77407751+Thermidor64@users.noreply.github.com>
2022-07-14 14:16:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import snowflake.connector
 
 streamlit.title('My Parents\' New Healthy Diner')
 
@@ -38,4 +39,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-# import snowflake.connector
+
